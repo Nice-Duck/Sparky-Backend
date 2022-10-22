65c5e48c761601a185d4c16d1e557bd68bafe998
myeongju-kim <mjoo1106@naver.com>
2022-10-22 18:47:15 +0900

    feat: modify response name(success -> server)

--- sparky/src/main/java/com/cmc/sparky/user/controller/UserController.java
+++ sparky/src/main/java/com/cmc/sparky/user/controller/UserController.java
@@ -1,11 +1,8 @@
 package com.cmc.sparky.user.controller;
 
-import com.cmc.sparky.common.dto.SuccessResponse;
-import com.cmc.sparky.user.repository.UserRepository;
+import com.cmc.sparky.common.dto.ServerResponse;
 import com.cmc.sparky.user.service.UserService;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +13,7 @@ public class UserController {
     private final UserService userService;
     @ApiOperation(value="중복확인",notes = "<strong>닉네임 중복을 확인한다.</strong>")
     @RequestMapping(value="/api/v1/users", method = RequestMethod.GET)
-    public ResponseEntity<SuccessResponse> userRegister(@RequestParam("name") String name){
+    public ResponseEntity<ServerResponse> userRegister(@RequestParam("name") String name){
         return ResponseEntity.ok().body(userService.dupName(name));
     }
 
