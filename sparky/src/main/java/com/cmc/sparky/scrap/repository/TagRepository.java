f30f78ce745314f2fb8455ff02b097463ed1d1f9
myeongju-kim <mjoo1106@naver.com>
2022-10-26 16:13:29 +0900

    fix: modify tag query search (add user information)

--- sparky/src/main/java/com/cmc/sparky/scrap/repository/TagRepository.java
+++ sparky/src/main/java/com/cmc/sparky/scrap/repository/TagRepository.java
@@ -1,10 +1,14 @@
 package com.cmc.sparky.scrap.repository;
 
 import com.cmc.sparky.scrap.domain.Tag;
+import com.cmc.sparky.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    Tag findByName(String name);
+    Tag findByNameAndUser(String name, User user);
+    List<Tag> findAllByUserOrderByIdDesc(User user);
 }
