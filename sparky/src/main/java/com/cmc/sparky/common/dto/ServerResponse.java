f3420032cccde91667310ec57453fed323810db4
myeongju-kim <mjoo1106@naver.com>
2022-10-29 15:14:12 +0900

    feat: modify common response

--- sparky/src/main/java/com/cmc/sparky/common/dto/ServerResponse.java
+++ sparky/src/main/java/com/cmc/sparky/common/dto/ServerResponse.java
@@ -1,12 +1,29 @@
 package com.cmc.sparky.common.dto;
 
+import com.cmc.sparky.common.exception.ErrorCode;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Data
 public class ServerResponse {
-    private String code="0000";
+    private String code;
     private String message;
-    private Object result=null;
+    private Object result;
+    public ServerResponse() {
+        this.code = "0000";
+    }
+    public ServerResponse(ErrorCode code, Object result) {
+        this.message = code.getMessage();
+        this.code = code.getCode();
+        this.result = result;
+    }
+    public ServerResponse success(String message, Object result){
+        this.message=message;
+        this.result=result;
+        return this;
+    }
+    public static ServerResponse of(ErrorCode code) {
+        return new ServerResponse(code, null);
+    }
 }
