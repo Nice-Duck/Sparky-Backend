464e6b43f574aaf1ad06f543f4ce7fa77907df55
myeongju-kim <mjoo1106@naver.com>
2022-11-01 13:49:12 +0900

    fix: modify user response (swagger)

--- sparky/src/main/java/com/cmc/sparky/user/controller/UserController.java
+++ sparky/src/main/java/com/cmc/sparky/user/controller/UserController.java
@@ -13,7 +13,7 @@ public class UserController {
     private final UserService userService;
     @ApiOperation(value="중복확인",notes = "<strong>닉네임 중복을 확인한다.</strong>")
     @RequestMapping(value="/api/v1/users", method = RequestMethod.GET)
-    public ResponseEntity<ServerResponse> userRegister(@RequestParam("name") String name){
+    public ResponseEntity<ServerResponse<Void>> userRegister(@RequestParam("name") String name){
         return ResponseEntity.ok().body(userService.dupName(name));
     }
 
