cfe480e19fc367a9da71e4d265f722f65f887ab1
myeongju-kim <mjoo1106@naver.com>
2022-11-01 13:48:43 +0900

    fix: modify account response (swagger)

--- sparky/src/main/java/com/cmc/sparky/account/controller/AccountController.java
+++ sparky/src/main/java/com/cmc/sparky/account/controller/AccountController.java
@@ -7,6 +7,7 @@ import com.cmc.sparky.account.dto.MailSendRequest;
 import com.cmc.sparky.account.service.AccountService;
 import com.cmc.sparky.account.service.MailService;
 import com.cmc.sparky.common.dto.ServerResponse;
+import com.cmc.sparky.common.dto.TokenDto;
 import com.cmc.sparky.common.service.JwtService;
 import com.cmc.sparky.scrap.dto.TagRequest;
 import io.swagger.annotations.ApiOperation;
@@ -22,34 +23,34 @@ public class AccountController {
     private final JwtService jwtService;
     @ApiOperation(value="중복확인",notes = "<strong>이메일 중복을 확인한다.</strong>")
     @RequestMapping(value="/api/v1/accounts/register", method = RequestMethod.GET)
-    public ResponseEntity<ServerResponse> duplicationUser(@RequestParam("email") String email){
+    public ResponseEntity<ServerResponse<Void>> duplicationUser(@RequestParam("email") String email){
         return ResponseEntity.ok().body(accountService.dupUser(email));
     }
     @ApiOperation(value="인증 메일 전송",notes = "<strong>인증메일전송</strong>")
     @RequestMapping(value="/api/v1/accounts/mails/send", method = RequestMethod.POST)
-    public ResponseEntity<ServerResponse> checkUser(@RequestBody MailSendRequest mailSendRequest) {
+    public ResponseEntity<ServerResponse<Void>> checkUser(@RequestBody MailSendRequest mailSendRequest) {
         return ResponseEntity.ok().body(mailService.sendMail(mailSendRequest));
     }
     @ApiOperation(value="인증 메일 확인",notes = "<strong>인증메일확인</strong>")
     @RequestMapping(value="/api/v1/accounts/mails/confirm", method = RequestMethod.POST)
-    public ResponseEntity<ServerResponse> confirmUser(@RequestBody MailCheckRequest mailCheckRequest){
+    public ResponseEntity<ServerResponse<Void>> confirmUser(@RequestBody MailCheckRequest mailCheckRequest){
         return ResponseEntity.ok().body(mailService.checkMail(mailCheckRequest));
     }
     @ApiOperation(value="회원가입",notes = "<strong>이메일과 패스워드를 입력받아 회원 가입을 진행한다.</strong>")
     @RequestMapping(value="/api/v1/accounts/register", method = RequestMethod.POST)
-    public ResponseEntity<ServerResponse> joinUser(@RequestBody AuthRequest authRequest){
+    public ResponseEntity<ServerResponse<TokenDto>> joinUser(@RequestBody AuthRequest authRequest){
         return ResponseEntity.ok().body(accountService.joinUser(authRequest));
     }
 
     @ApiOperation(value="로그인",notes = "<strong>이메일과 패스워드를 입력받아 성공 여부를 알린다.</strong>")
     @RequestMapping(value="/api/v1/accounts", method = RequestMethod.POST)
-    public ResponseEntity<ServerResponse> loginUser(@RequestBody LoginRequest loginRequest){
+    public ResponseEntity<ServerResponse<TokenDto>> loginUser(@RequestBody LoginRequest loginRequest){
         return ResponseEntity.ok().body(accountService.checkUser(loginRequest));
     }
 
     @ApiOperation(value="회원탈퇴",notes = "<strong>회원 탈퇴</strong>")
     @RequestMapping(value="/api/v1/accounts", method = RequestMethod.DELETE)
-    public ResponseEntity<ServerResponse> deleteUser(@RequestHeader("Authorization") String token){
+    public ResponseEntity<ServerResponse<Void>> deleteUser(@RequestHeader("Authorization") String token){
         jwtService.validateToken(token);
         String email=jwtService.getUser(token);
         return ResponseEntity.ok().body(accountService.outUser(email));
