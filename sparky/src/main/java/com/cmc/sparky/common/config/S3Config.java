35823a8d2a64eb1d5f18d98b98f32bdfa20f64dc
myeongju-kim <mjoo1106@naver.com>
2022-11-02 11:57:05 +0900

    feat: create s3 config and gradle

---
+++ sparky/src/main/java/com/cmc/sparky/common/config/S3Config.java
@@ -0,0 +1,28 @@
+package com.cmc.sparky.common.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+    @Value("${cloud.aws.client}")
+    private String iamAccessKey; // IAM Access Key
+    @Value("${cloud.aws.password}")
+    private String iamSecretKey; // IAM Secret Key
+    @Value("${cloud.aws.region.static}")
+    private String region; // Bucket Region
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(iamAccessKey, iamSecretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .build();
+    }
+}
